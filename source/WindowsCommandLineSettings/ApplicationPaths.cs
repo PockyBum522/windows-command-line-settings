5e87b5fb8d49897611b6d5df187063e4527d8577
DSikes <david@sikespc.com>
2024-03-25 08:22:31 -0400

    Initial commit

---
+++ source/WindowsCommandLineSettings/ApplicationPaths.cs
@@ -0,0 +1,65 @@
+﻿namespace WindowsCommandLineSettings
+{
+    /// <summary>
+    /// Contains the few paths for this application that must be hardcoded
+    /// </summary>
+    public static class ApplicationPaths
+    {
+        /// <summary>
+        /// Per-user log folder path
+        /// </summary>
+        private static string LogAppBasePath =>
+            Path.Combine(
+                "C:",
+                "Users",
+                "Public",
+                "Documents",
+                "Logs",
+                "WindowsCommandLineSettings");
+
+        /// <summary>
+        /// Actual log file path passed to the ILogger configuration
+        /// </summary>
+        public static string LogPath =>
+            Path.Combine(
+                LogAppBasePath,
+                "log_.log");
+        
+        /// <summary>
+        /// The directory the assembly is running from
+        /// </summary>
+        public static string ThisApplicationRunFromDirectoryPath => 
+            Path.GetDirectoryName(Environment.ProcessPath) ?? "";
+
+        /// <summary>
+        /// The top level dir, useful for getting to configuration folders and resource folders
+        /// This is the directory the bootstrapper bat file is in
+        /// </summary>
+        public static string SetupAssistantRootDir => 
+            Path.GetFullPath(
+                Path.Join(
+                    ThisApplicationRunFromDirectoryPath, "../../../../.."));
+        
+        /// <summary>
+        /// The full path to this application's running assembly
+        /// </summary>
+        public static string ThisApplicationProcessPath => 
+            Environment.ProcessPath ?? "";
+
+        /// <summary>
+        /// Where to put the JSON file representing what state the setup is in, state is based on user selection in
+        /// MainWindow
+        /// </summary>
+        public static string StatePath => @"C:\Users\Public\Documents\state.json";
+
+        /// <summary>
+        /// Path to the file containing registry files and ps1 scripts
+        /// </summary>
+        public static string ResourceDirectoryExternalFiles =>
+            Path.GetFullPath(
+                Path.Join(
+                    SetupAssistantRootDir,
+                    "resources"));
+
+    }
+}
---
+++ source/WindowsCommandLineSettings/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace WindowsCommandLineSettings;
+
+internal static class Program
+{
+    public static void Main(string[] args)
+    {
+        if (args.Length < 2)
+        {
+            PrintHelpToConsole();
+            
+            return;
+        }
+        
+        Console.WriteLine("Hello, World!");
+    }
+
+    private static void PrintHelpToConsole()
+    {
+        var message = @"
+This help is printed when windowsCommandLineSettings.exe is run with no arguments or with -help
+
+To print this help:
+-help
+
+To see available settings that can be changed:
+-commands
+
+To get extended help on a specific setting change:
+-help setting-name
+
+To explicitly set log file location (Timestamp will be appended to log name automatically):
+-logpath ""C:\Users\Public\Documents\Logs\WindowsCommandLineSettings\log_.log"" 
+Note: The default log path is the above path";
+
+        Console.WriteLine();
+    }
+}
