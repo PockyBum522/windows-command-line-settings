14df6494e89617b1c6fa912802af304a8ac2b720
DSikes <david@sikespc.com>
2024-04-20 12:27:45 -0400

    Added logger builder

---
+++ source/WindowsCommandLineSettings/DependencyBuilders.cs
@@ -0,0 +1,25 @@
+﻿using Serilog;
+
+namespace WindowsCommandLineSettings;
+
+public static class DependencyBuilders
+{
+    internal static ILogger BuildLogger(string logPath, ILogger? testingLogger = null)
+    {
+        Directory.CreateDirectory(Path.GetDirectoryName(ApplicationPaths.LogPath) ?? "");
+
+        if (testingLogger is not null)
+        {
+            return testingLogger;
+        }
+
+        // Otherwise, if it is null, make new logger:
+        return new LoggerConfiguration()
+            .Enrich.WithProperty("WindowsSetupAssistantApplication", "WindowsSetupAssistantSerilogContext")
+            .MinimumLevel.Debug()
+            .WriteTo.File(logPath, rollingInterval: RollingInterval.Day)
+            .WriteTo.Console()
+            .WriteTo.Debug()
+            .CreateLogger();
+    }
+}
